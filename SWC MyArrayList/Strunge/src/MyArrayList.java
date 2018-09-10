640048f2bf496a9cd48c6bd344e862982ed05957
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2018-09-10 09:59:31 +0200

    Updt. ArrayList

--- SWC MyArrayList/Strunge/src/MyArrayList.java
+++ SWC MyArrayList/Strunge/src/MyArrayList.java
@@ -8,7 +8,7 @@ public class MyArrayList<T> implements MyList {
     @Override
     //Check for current length
     public int size() {
-        return data.length;
+        return size;
     }
 
     @Override
@@ -17,7 +17,7 @@ public class MyArrayList<T> implements MyList {
         //Check if there is anymore room in the list
         if (data.length - size < 1){
             //Copies old array, and adds its indexes to a new array, which is longer
-            data = Arrays.copyOf(data, data.length*2);
+            data = Arrays.copyOf(data, data.length + 10);
         }
         //Increments size, and sets the element as the same as size, at size 5 it does (5*2) so that Object[5] becomes Object[10]
         data[size++] = (T) element;
@@ -26,53 +26,28 @@ public class MyArrayList<T> implements MyList {
     @Override
     public Object remove(int index) {
         //Checks if the index is within the array
-        if (index < size){
-            Object obj = data[index];
-            int temp = index;
-            while(temp < size){
-                data[temp] = data[temp+1];
-                temp++;
+        Object obj = null;
+        if (index < size) {
+            obj = data[index];
+            while (index < size) {
+                data[index] = data[index + 1];
+                index++;
             }
             //Reduces the size, since an index has been removed
             size--;
             //Returns what to remove
-            return obj;
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
+
         }
+        return obj;
     }
 
     @Override
     //Gets the element at the target index (in this case using "for" loop)
-    public Object get(int index) {
+    public Object get(int index){
         //See lin. 28
         if (index < size){
             return data[index];
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
         }
+        else return null;
     }
-
-    public static void main(String[] args){
-
-        MyArrayList test = new MyArrayList();
-        test.add(10);
-        test.add(20);
-        test.add(30);
-        test.add(40);
-        test.add(50);
-        test.add(60);
-        test.add(70);
-        for (int i = 0; i < test.size; i++){
-            System.out.print(test.get(i)+ " ");
-        }
-        System.out.println("Length of current ArrayList : " + test.size);
-        System.out.println("Check index 3 : " + test.get(3) + " Is at index 3.");
-        System.out.println("Removing the element at index 4, which is : " + test.remove(4) + "\nAnd hereafter, check the arraylist again.");
-        for (int i = 0; i < test.size; i++){
-            System.out.print(test.get(i) + " ");
-        }
-        System.out.println("\nAmount of indexes : " + test.size);
-    }
-
 }
