bd6c738a0acfdec3ea4320d8400f5c38e4feb279
C-Strunge <33119378+C-Strunge@users.noreply.github.com>
2018-09-12 17:54:03 +0200

    Arraylist

--- SWC MyArrayList/Strunge/src/MyArrayList.java
+++ SWC MyArrayList/Strunge/src/MyArrayList.java
@@ -17,7 +17,7 @@ public class MyArrayList<T> implements MyList {
         //Check if there is anymore room in the list
         if (data.length - size < 1){
             //Copies old array, and adds its indexes to a new array, which is longer
-            data = Arrays.copyOf(data, data.length + 10);
+            data = Arrays.copyOf(data, data.length + 5);
         }
         //Increments size, and sets the element as the same as size, at size 5 it does (5*2) so that Object[5] becomes Object[10]
         data[size++] = (T) element;
@@ -28,7 +28,7 @@ public class MyArrayList<T> implements MyList {
         //Checks if the index is within the array
         Object obj = null;
         if (index < size) {
-        obj = data[index];
+            obj = data[index];
             while (index < size) {
                 data[index] = data[index + 1];
                 index++;
