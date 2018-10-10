178876d6454bffd9f180e430852d08e7618a0d38
Patrick <Patricksirich@hotmail.com>
2018-10-10 18:45:48 +0200

    server exception ved quit (virker ikke helt)

--- Patrick/TCPUPD/src/SWCUDPTCP/TCPServer.java
+++ Patrick/TCPUPD/src/SWCUDPTCP/TCPServer.java
@@ -24,8 +24,10 @@ public class TCPServer {
             ServerSocket server = new ServerSocket(PORT_LISTEN);
             System.out.println("Server starting...\n");
 
-            while (true) {
 
+
+            while (true) {
+                Client client = new Client();
                 socket = server.accept();
                 System.out.println("Client connected");
 
@@ -55,11 +57,17 @@ public class TCPServer {
                     OutputStream acceptSocket = null;
                     try {
                         acceptSocket = socket.getOutputStream();
+                        input = socket.getInputStream();
                         output = socket.getOutputStream();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    users.add(new Client(USERNAME, socket, input, output));
+                    client.setName(USERNAME);
+                    client.setSocket(socket);
+                    client.setOutput(output);
+                    client.setInput(input);
+
+                    users.add(client);
                     String clientAccept = "J_OK";
                     System.out.println(users);
                     System.out.println("JOIN " + USERNAME + ", " + clientIp + ":" + PORT_LISTEN);
@@ -69,59 +77,86 @@ public class TCPServer {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    String ClientList = "";
-                    for (Client c: users) {
-                        ClientList += c.getName() + ", ";
+
+
+                    try {
+                        ListOfClients(users);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                    for (Client c: users) {
-                        output = c.getOutput();
-                        String cList = "List of all clients: " + ClientList;
-                        byte[] listofclients;
-                        listofclients = cList.getBytes();
+
+
+//                    String ClientList = "";
+//                    for (Client c : users) {
+//                        ClientList += c.getName() + ", ";
+//                    }
+//                    for (Client c : users) {
+//                        output = c.getOutput();
+//                        String cList = "List of all clients: " + ClientList;
+//                        byte[] listofclients;
+//                        listofclients = cList.getBytes();
+//                        try {
+//                            output.write(listofclients);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+
+
+                    do {
                         try {
-                            output.write(listofclients);
+                            input = socket.getInputStream();
+                            output = socket.getOutputStream();
+
+                            byte[] dataIn = new byte[1024];
+                            input.read(dataIn);
+                            String msgIn = new String(dataIn);
+                            msgIn = msgIn.trim();
+
+
+
+                            if (msgIn.equalsIgnoreCase("!quit")) {
+                                users.remove(client);
+                                socket.close();
+                                ListOfClients(users);
+                                break;
+                            }
+                            for (Client c : users) {
+
+                                if (msgIn.trim().length() > 250) {
+                                    byte[] J_ER_TooLong;
+                                    String J_ER_long = "J_ER MESSAGE TOO LONG: Message contains: " + msgIn.length() + " characters, max length is 250";
+                                    J_ER_TooLong = J_ER_long.getBytes();
+                                    output.write(J_ER_TooLong);
+                                } else if (!msgIn.equals("IMAV") && !msgIn.contains("!Quit")) {
+                                    output = c.getOutput();
+                                    output.write(dataIn);
+                                } else {
+                                    System.out.println(msgIn);
+                                }
+                                System.out.println(msgIn);
+                            }
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                    }
-
-
-
-                     do {
-                         try {
-                             input = socket.getInputStream();
-                             output = socket.getOutputStream();
-
-                             byte[] dataIn = new byte[1024];
-                             input.read(dataIn);
-                             String msgIn = new String(dataIn);
-                             msgIn = msgIn.trim();
-
-                             for (Client c : users) {
-
-                                     if (msgIn.trim().length() > 250) {
-                                         byte[] J_ER_TooLong;
-                                         String J_ER_long = "J_ER MESSAGE TOO LONG: Message contains: " + msgIn.length() + " characters, max length is 250";
-                                         J_ER_TooLong = J_ER_long.getBytes();
-                                         output.write(J_ER_TooLong);
-                                     }else if(!msgIn.equals("IMAV")){
-                                         output = c.getOutput();
-                                         output.write(dataIn);
-                                     }else{System.out.println(msgIn);
-                                     }
-
-                             }
-                             System.out.println(msgIn);
-
-                         } catch (IOException e) {
-                             e.printStackTrace();
-                         }
-                     }while (!socket.isClosed());
+                    } while (!socket.isClosed());
                 });
                 t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    static void ListOfClients(ArrayList<Client> users) throws IOException {
+
+        String clientList = "";
+        for (Client c : users) {
+            clientList += c.getName() + ", ";
+            output = c.getOutput();
+            String cList = "List of all clients: " + clientList.substring(0, clientList.lastIndexOf(","));
+            byte[] listofclients;
+            listofclients = cList.getBytes();
+            output.write(listofclients);
         }
     }
 
