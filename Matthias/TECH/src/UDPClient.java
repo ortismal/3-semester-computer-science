e4ce1dc803c89019b7a8bdea9bc2269bc195e581
MatthiasSkou <ms@klinikskou.dk>
2018-10-27 17:39:08 +0200

    UDP done

--- Matthias/TECH/src/UDPClient.java
+++ Matthias/TECH/src/UDPClient.java
@@ -1,31 +1,52 @@
 import java.net.*;
 import java.util.Scanner;
 
-public class UDPClient {
+    /**
+     * Take	the	two	exmple	programs	“UDP_11.java” and	“UDP_00.java” and	change	them	so	that
+     * they	work,	and	the	two	users	can	send	messages	back	and	forth	for	ever,	until	one	of	them
+     * sends	the	message	“quit”,	then	both	sides	shut	down
+     */
 
-    public static void main(String[] args) throws Exception {
 
-        String sentence;
-        int length;
+/**
+ * Write a description of class UDP_1 here.
+ *
+ */
 
-        Scanner inFromKbd = new Scanner(System.in);
-        DatagramSocket receivingSocket = new DatagramSocket(6710);
-        DatagramSocket sendingSocket = new DatagramSocket();
-        InetAddress IPAdress = InetAddress.getByName("10.111.176.164");
-        byte[] data = new byte[1024];
+    public class UDPClient {
+        public static void main(String args[]) throws Exception {
 
-        System.out.println("Please type your message: ");
-        sentence = inFromKbd.nextLine();
-        length = sentence.length();
-        data = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(data,  length, IPAdress, 6710);
-        sendingSocket.send(sendPacket);
-        sentence = "            ";
-        data = sentence.getBytes();
+            Scanner sc = new Scanner(System.in);
+            final int PORT_IN = 5656;
+            final int PORT_OUT = 5757;
+            DatagramSocket receivingSocket = new DatagramSocket(PORT_IN);
 
-        DatagramPacket receivePackage = new DatagramPacket(data, data.length);
-        receivingSocket.receive(receivePackage);
-        sentence = new String(receivePackage.getData());
-        System.out.println("FROM SERVER: " + sentence);
+            do {
+                InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+                byte[] data = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+
+                System.out.println("Please type your message: ");
+                String msgToSend = sc.nextLine();
+                data = msgToSend.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, PORT_OUT);
+                receivingSocket.send(sendPacket);
+                if(msgToSend.equals("quit")){
+                    break;
+                }
+
+                receivingSocket.receive(receivePacket);
+                String msgIn = new String(receivePacket.getData());
+
+                if(msgIn.equals("quit")) {
+                    System.out.println("FROM SERVER: " + msgIn + " - Shutting down!");
+                    break;
+                }
+                System.out.println("FROM SERVER:" + msgIn);
+
+            } while (true);
+        }
     }
-}
+
+
--- Matthias/TECH/src/UDPServer.java
+++ Matthias/TECH/src/UDPServer.java
@@ -1,43 +1,51 @@
-
-
-
 /**
  * Write a description of class UDP_0 here.
  *
  */
 
-import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+    /**
+     * Take	the	two	exmple	programs	“UDP_11.java” and	“UDP_00.java” and	change	them	so	that
+     * they	work,	and	the	two	users	can	send	messages	back	and	forth	for	ever,	until	one	of	them
+     * sends	the	message	“quit”,	then	both	sides	shut	down
+     */
 
-public class UDPServer {
-    public static void main(String args[]) throws Exception {
+    public class UDPServer {
+        public static void main(String args[]) throws Exception {
 
-        String sentence;
-        int length;
+            Scanner sc = new Scanner(System.in);
+            final int PORT_IN = 5757;
+            final int PORT_OUT = 5656;
+            DatagramSocket receivingSocket = new DatagramSocket(PORT_IN);
 
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket receivingSocket = new DatagramSocket(6710);
-        DatagramSocket sendingSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("10.111.176.164");
-        byte[] data = new byte[1024];
+            DatagramSocket sendingSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
 
-        DatagramPacket receivePacket = new DatagramPacket(data, 33);
-        receivingSocket.receive(receivePacket);
-        sentence = new String(receivePacket.getData());
-        int size = receivePacket.getLength();
-        System.out.println("FROM SERVER size:" + size);
-        System.out.println("FROM SERVER:" + sentence);
+            do {
 
-        System.out.println("Please type you message: ");
-        sentence = inFromUser.readLine();
-        length = sentence.length();
-        data = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(data, length, IPAddress, 6710);
-        sendingSocket.send(sendPacket);
-        sentence = " ";
-        data = sentence.getBytes();
+                byte[] data = new byte[1024];
 
+                DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+                receivingSocket.receive(receivePacket);
+                String msgIn = new String(receivePacket.getData());
+                if(msgIn.equals("quit")){
+                    System.out.println("FROM CLIENT: " + msgIn + " - Shutting down!");
+                    break;
+                }
+
+                int size = receivePacket.getLength();
+                System.out.println("FROM CLIENT size:" + size);
+                System.out.println("FROM CLIENT:" + msgIn);
+
+                System.out.println("Please type your message: ");
+                String msgToSend = sc.nextLine();
+                data = msgToSend.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPAddress, PORT_OUT);
+                sendingSocket.send(sendPacket);
+            } while(true);
+        }
     }
-}
+
 
 
