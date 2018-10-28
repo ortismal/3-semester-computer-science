9996898b6ae601496ac3d2650df1b8065b4b78c1
Patricksirich <Patricksirich@hotmail.com>
2018-10-28 12:46:12 +0100

    WebServer almost done

--- Patrick/TCPUPD/src/Webserver/WebServer_serial.java
+++ Patrick/TCPUPD/src/Webserver/WebServer_serial.java
@@ -6,41 +6,34 @@ import java.time.LocalDateTime;
 import java.util.*;
 import java.lang.*;
 
-public class WebServer_serial
-{
+public class WebServer_serial {
+    static Thread client;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("OK, we are starting the WebServer.");
 
-        try
-        {
+        try {
             ServerSocket listnerSocket = new ServerSocket(42424);
             System.out.println("OK, we have a listening socket.");
 
-            while(true)
-            {
+            while (true) {
                 Socket newsocket = listnerSocket.accept();
                 System.out.println("OK, we got a client connection!");
                 ServiceTheClient(newsocket);
             }
 
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Webserver IO exception");
         }
 
     }
 
 
-    public static void ServiceTheClient(Socket con)
-    {
+    public static void ServiceTheClient(Socket con) {
         Socket socket;
         socket = con;
 
-        try
-        {
+        try {
             System.out.println("****************************************************************************");
             System.out.println("OK, we are starting to service the client.");
             String path = "\\Users\\patri\\Desktop\\3-semester-computer-science\\Patrick\\TCPUPD\\src\\Webserver\\";
@@ -55,61 +48,63 @@ public class WebServer_serial
 
             StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
 
-            if(tokenizedLine.nextToken().equals("GET"))
-            {
+            if (tokenizedLine.nextToken().equals("GET")) {
                 fileName = tokenizedLine.nextToken();
 
-                if(fileName.startsWith("/") == true)
-                {
+                if (fileName.startsWith("/") == true) {
                     fileName = path + fileName;
                 }
 
-                if(fileName.endsWith("/") == true)
-                {
+                if (fileName.endsWith("/") == true) {
                     fileName = fileName + "index.html";
                 }
 
                 File file = new File(fileName);
-                if (!file.isFile())
-                {
+                if (!file.isFile()) {
                     fileName = path + "error404.html";
                     file = new File(fileName);
                 }
 
                 System.out.println("Trying to find file: " + fileName);
-
-                int numOfBytes = (int)file.length();
+                int numOfBytes = (int) file.length();
                 FileInputStream inFile = new FileInputStream(fileName);
                 byte[] fileInBytes = new byte[numOfBytes];
-                inFile.read(fileInBytes);
-                inFile.close();  //***** remember to close the file after usage *****
+
                 outToClient.writeBytes("HTTP/1.0 200: Good request\r\n");
                 outToClient.writeBytes("Date: " + LocalDateTime.now() + "\r\n");
                 outToClient.writeBytes("Server: Patrick\r\n");
-                outToClient.writeBytes(("Content-Type: " + fileName.substring(fileName.lastIndexOf(").")));
+                outToClient.writeBytes("Content-Type: " + fileName.substring(fileName.lastIndexOf(".")));
                 outToClient.writeBytes("Content-Length: " + numOfBytes + "\r\n");
                 outToClient.writeBytes("\r\n");
-                outToClient.write(fileInBytes, 0, numOfBytes);
+                int off = 0;
+                while (numOfBytes > off + 16000) {
+                    int bytesRead = inFile.read(fileInBytes, off, 16000);
+                    outToClient.write(fileInBytes, off, 16000);
+                    System.out.println(off);
+                    off += bytesRead;
+                }
+                System.out.println("Off size: " + off);
+                inFile.read(fileInBytes, off, numOfBytes - off);
+                inFile.close();//***** remember to close the file after usage *****
+                outToClient.write(fileInBytes, off, numOfBytes - off);
                 outToClient.writeBytes("\n");
+
 
                 System.out.println("OK, the file is sent to Client.");
                 System.out.println("****************************************************************************");
 
                 socket.close();
-            }
-            else // no "GET"
+            } else // no "GET"
             {
                 System.out.println("Bad request Message");
                 outToClient.writeBytes("HTTP/1.0 ERROR 500: NO GET REQUEST\r\n");
                 outToClient.writeBytes("\n");
                 socket.close();
             }
-        }
-
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("IO Exception");
         }
+
 
     }  // end of
 
